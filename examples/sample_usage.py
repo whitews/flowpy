46ccdb8e7518c8b6d4ab229351c70256e2e301c0
Scott White <whitews@gmail.com>
2018-01-03 05:47:57 -0500

    update sample usage

--- examples/sample_usage.py
+++ examples/sample_usage.py
@@ -1,24 +1,5 @@
 from flowpy import Sample
 
-# import numpy as np
-#
-# a = np.array(
-#     [
-#         [0, 1, 2, 3],
-#         [0, 1, 2, 3],
-#         [0, 1, 2, 3],
-#         [0, 1, 2, 3],
-#         [0, 1, 2, 3],
-#     ],
-#     dtype=np.float
-# )
-#
-# indices = [3, 2]
-# new_a = a.copy()[:, indices]
-# new_a += 0.1
-# a[:, indices] = new_a
-# print(new_a.shape)
-
 comp_matrix = """Blue B-A	Blue A-A	Violet H-A	Violet G-A	Red C-A	Red B-A	Red A-A	Green E-A	Green D-A	Green C-A	Green A-A
 1	7.5e-3	0	0.0414	0	0	0	9.e-3	7.5e-3	0	0
 -0.01	1	0	0.035	0.02	0.12	-0.045	0	0.01	0.145	0.055
@@ -32,10 +13,7 @@ comp_matrix = """Blue B-A	Blue A-A	Violet H-A	Violet G-A	Red C-A	Red B-A	Red A-A
 0	0.29	0	0	0.091	0.084	0.028	0.0162	0.0174	1	0.093
 0	0.01	-5.e-4	-5.e-3	-1.e-3	0	0.0885	0.023	0.0225	4.53e-3	1"""
 
-s = Sample("K690194C-01_Costim.fcs")
+s = Sample("../K690194C-01_Costim.fcs")
 s.generate_subsample(1000, random_seed=123)
 s.compensate_events(comp_matrix)
-s.transform_logicle()
-
-print(s)
-
+s.transform_asinh()
